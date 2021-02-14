d82790c46700f8695719278fdb7e1d7a9c4e4178
Jack <29169102+Jack5079@users.noreply.github.com>
2021-02-14 17:52:46 -0500

    better update?

--- commands/system/update.ts
+++ commands/system/update.ts
@@ -9,89 +9,91 @@ const shell = (str: string) => new Promise<string>((resolve, reject) => {
     resolve(stdout)
   })
 })
-export async function run (
-  this: Bot,
-  message: Message
-): Promise<void> {
-  if (message.author.id === process.env.OWNER) {
-    const start = new Date
-    const brand = `${this.client.user?.username || 'trollsmile'} update`
-    const msg = await message.channel.send({
+interface Step {
+  name: string
+  run (this: Bot): void | Promise<void>
+}
+
+async function update (this: Bot, message: Message, steps: Step[]) {
+  const brand = `${this.client.user?.username || 'trollsmile'} update`
+  const start = new Date
+  const msg = await message.channel.send({
+    embed: {
+      author: {
+        name: brand,
+        icon_url: this.client.user?.avatarURL() || undefined
+      },
+      title: 'Update starting...'
+    }
+  })
+
+  for (const step of steps) {
+    msg.edit({
       embed: {
         author: {
           name: brand,
           icon_url: this.client.user?.avatarURL() || undefined
         },
-        title: 'Downloading latest trollsmile...'
+        title: step.name
       }
     })
-    try {
-      await shell('git fetch origin main')
-      await shell('git reset --hard origin/main')
-      msg.edit({
-        embed: {
-          author: {
-            name: brand,
-            icon_url: this.client.user?.avatarURL() || undefined
-          },
-          title: 'Updating dependencies...'
-        }
-      })
-      await shell('npm i')
-      msg.edit({
-        embed: {
-          author: {
-            name: brand,
-            icon_url: this.client.user?.avatarURL() || undefined
-          },
-          title: 'Compiling...'
-        }
-      })
-      await shell(
-        platform() === 'win32'
-          ? 'PowerShell -Command "rm commands/**/*.js"'
-          : 'rm commands/**/*.js'
-      ) // remove previous files because what if i deleted a command
-      await shell('npx tsc')
-      msg.edit({
-        embed: {
-          author: {
-            name: brand,
-            icon_url: this.client.user?.avatarURL() || undefined
-          },
-          title: 'Reloading all commands...'
-        }
-      })
-      this.commands.clear()
-      this.aliases.clear()
-      this.load_cmds()
-      msg.edit({
-        embed: {
-          author: {
-            name: brand,
-            icon_url: this.client.user?.avatarURL() || undefined
-          },
-          color: 'GREEN',
-          title: `Update complete! Took ${(new Date().getTime() - start.getTime())}ms`,
-          description: 'Restart the bot to reload events and messages.'
-        }
-      })
-    } catch (err) {
-      if (typeof err === 'string') {
-        msg.edit({
-          embed: {
-            author: {
-              name: brand,
-            },
-            title: 'Error!',
-            description: '```\n' + err + '\n```',
-            color: 'RED'
-          }
-        })
-      }
+    await step.run.call(this)
+  }
+  msg.edit({
+    embed: {
+      author: {
+        name: brand,
+        icon_url: this.client.user?.avatarURL() || undefined
+      },
+      color: 'GREEN',
+      title: `Update complete! Took ${(new Date().getTime() - start.getTime())}ms`,
+      description: 'Restart the bot to reload events and messages.'
     }
+  })
+}
+
+export async function run (
+  this: Bot,
+  message: Message
+): Promise<void> {
+  if (message.author.id === process.env.OWNER) {
+    update.call(this, message, [
+      {
+        name: 'Downloading latest trollsmile...',
+        async run () {
+          await shell('git fetch origin main')
+          await shell('git reset --hard origin/main')
+        }
+      },
+      {
+        name: 'Updating dependencies...',
+        async run () {
+          await shell('npm i')
+        }
+      },
+      {
+        name: 'Compiling...',
+        async run () {
+          await shell(
+            platform() === 'win32'
+              ? 'PowerShell -Command "rm commands/**/*.js"'
+              : 'rm commands/**/*.js'
+          ) // remove previous files because what if i deleted a command
+          await shell('npx tsc')
+        }
+      },
+      {
+        name: 'Reloading all commands...',
+        async run () {
+          this.commands.clear()
+          this.aliases.clear()
+          await this.load_cmds()
+        }
+      }
+    ])
   } else {
     message.reply('You are not the bot owner.')
   }
 }
+
 export const help = 'Updates the bot.'
