76dec94bb24519b0d62236baf8e8e0e99905fae6
Jack <git@5079.ml>
2021-02-27 00:57:15 -0500

    use underscore so typescript shuts up

--- commands/internet/robloxip.ts
+++ commands/internet/robloxip.ts
@@ -1,7 +1,7 @@
 import { Message, MessageOptions } from 'discord.js'
 import type Bot from '../../init'
 import is_roblox from 'is-roblox-ip'
-export async function run (this: Bot, message: Message, args: string[]): Promise<MessageOptions> {
+export async function run (this: Bot, _message: Message, args: string[]): Promise<MessageOptions> {
   const ip_regex = /(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}/gm
   if (ip_regex.test(args.join('')) && is_roblox(args.join(''))) {
     return {
