fd9cedf3f6cfbc9e5a610c3b2b273c544071bd4f
Jack <git@5079.ml>
2021-02-27 01:05:37 -0500

    add a progress bar too

--- commands/system/update.ts
+++ commands/system/update.ts
@@ -71,6 +71,7 @@ async function update (this: Bot, channel: TextChannel | DMChannel | NewsChannel
           name: brand,
           icon_url: this.client.user?.avatarURL() || undefined
         },
+        description: '```\n' + '█'.repeat(((steps.indexOf(step) + 1) / steps.length) * 10).padEnd(10, '▒') + '\n```',
         title: step.name
       }
     })
