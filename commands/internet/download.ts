5b09b0c14fa2f9824bc77a4fa99d3461d529d996
Jack <git@5079.ml>
2021-06-26 21:38:42 -0400

    disable ytdlcore

--- commands/internet/download.ts
+++ commands/internet/download.ts
@@ -1,37 +1,45 @@
 import { Message, MessageOptions } from 'discord.js'
-import { createRequire } from "module"
+import { createRequire } from 'module'
 import fetch from 'node-fetch'
 const require = createRequire(import.meta.url)
 const { validateURL, getInfo, chooseFormat } = require('ytdl-core')
 
-export const run = async (message: Message, args: string[]): Promise<MessageOptions> => {
-  if (validateURL(args.join(' '))) {
+export const run = async (
+  message: Message,
+  args: string[]
+): Promise<MessageOptions> => {
+  if (false) {
+    // ytdl-core currently disabled
     const info = await getInfo(args.join(' '))
     const vid = chooseFormat(info.formats, {
       filter: 'audioandvideo',
-      quality: 'highest',
+      quality: 'highest'
     })
-    return vid ? {
-      embed: {
-        title: info.videoDetails.title,
-        author: {
-          name: info.videoDetails.ownerChannelName,
-          url: info.videoDetails.ownerProfileUrl
-        },
-        url: vid.url,
-        description: 'Click the above link to download.'
-      }
-    } : {
-        embed: {
-          title: "we couldn't find a video with video lol",
-          description: 'fuc kyou',
-          color: 'RED',
+    return vid
+      ? {
+          embed: {
+            title: info.videoDetails.title,
+            author: {
+              name: info.videoDetails.ownerChannelName,
+              url: info.videoDetails.ownerProfileUrl
+            },
+            url: vid.url,
+            description: 'Click the above link to download.'
+          }
         }
-      }
+      : {
+          embed: {
+            title: "we couldn't find a video with video lol",
+            description: 'fuc kyou',
+            color: 'RED'
+          }
+        }
   } else {
-    const video = await fetch(`https://projectlounge.pw/ytdl/download?url=${encodeURIComponent(
-      args.join(' ')
-    )}`).then(res => res.buffer())
+    const video = await fetch(
+      `https://projectlounge.pw/ytdl/download?url=${encodeURIComponent(
+        args.join(' ')
+      )}`
+    ).then((res) => res.buffer())
     return {
       embed: {
         title: 'Click here if the video is not displayed.',
@@ -39,10 +47,15 @@ export const run = async (message: Message, args: string[]): Promise<MessageOpti
           args.join(' ')
         )}`
       },
-      files: video.length <= 8388608 ? [{
-        attachment: video,
-        name: 'video.mp4'
-      }] : undefined
+      files:
+        video.length <= 8388608
+          ? [
+              {
+                attachment: video,
+                name: 'video.mp4'
+              }
+            ]
+          : undefined
     }
   }
 }
